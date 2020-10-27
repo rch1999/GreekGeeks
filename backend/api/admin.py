c6268633a6dd7cb25bd3594a8b718cad34ddba9b
Tim <tmclough98@gmail.com>
2020-10-27 15:39:41 -0400

    Change Member to User, add some dependencies, make JWT routes

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+import api.models as models
 
 # Register your models here.
+admin.site.register(models.Contact)
+admin.site.register(models.ContactMethod)
+admin.site.register(models.ContactNote)
+admin.site.register(models.Tag)
+admin.site.register(models.User)
+admin.site.register(models.Notification)
+admin.site.register(models.Organization)
+admin.site.register(models.Task)
+admin.site.register(models.OrganizationImage)
